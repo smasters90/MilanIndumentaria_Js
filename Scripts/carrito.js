e573ddca9a53cffba82616a03b2bed8e708695dc
= <=>
2022-08-24 18:04:48 -0300

    avances

--- Scripts/carrito.js
+++ Scripts/carrito.js
@@ -1,13 +1,18 @@
 function agregarCarrito(prod){
     
         contProduct.innerHTML += `
-            <br>
-            <img class="img--carrito  img-fluid" src="${prod.imagen}" alt="${prod.nombre} id = "${prod.id}"">
-            <span> Producto: ${prod.nombre} </span>
-            <span> Precio $: ${prod.precio} </span>
-            <span> Cantidad: ${prod.cantidad} </span>
-            <span> Total $: ${prod.precio * prod.cantidad } </span>
-            <br>
+            <div class = "contenedor__carrito d-flex flex-wrap gap-2 align-items-center justify-content-around">
+                <div class ="col-4">
+                    <img class="img--carrito  img-fluid d-flex" src="${prod.imagen}" alt="${prod.nombre} id = "${prod.id}"">
+                </div>
+                <div class = "col-6">
+                    <p> Producto: ${prod.nombre} </p>
+                    <p> Precio $: ${prod.precio} </p>
+                    <p> Cantidad: ${prod.cantidad} </p>
+                    <p> <strong> Total $: ${prod.precio * prod.cantidad } </strong></p>
+                </div>
+                <br>
+            </div>
             `
 }
 
@@ -19,7 +24,7 @@ function limpiarCarrito(){
 
 
 
-let contProduct = document.getElementById("cont__prod");
+let contProduct = document.getElementById("cont__producto");
 let botonCarrito = document.querySelector('.btn__carrito');
 
 botonCarrito.addEventListener('click', ()=>{
